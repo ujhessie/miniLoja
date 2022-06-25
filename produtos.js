b6b681eb14bd3ef2f92e663b549e777deeee0308
ujhessie <juniorrodrigues4502@gmail.com>
2022-06-25 19:57:47 -0300

    a

---
+++ produtos.js
@@ -0,0 +1,8 @@
+var produtos = [
+	// {desc: "", valor: "", cod: ""}
+	{desc: "Item 1", valor: "R$ 15, 00", cod: "01"},
+]
+
+var divProdutos = document.querySelector()
+
+console.log(produtos[0].desc)
