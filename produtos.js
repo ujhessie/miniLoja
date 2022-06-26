997c9d9b5506107a4b9d8e5d720935f8b5deeac0
ujhessie <juniorrodrigues4502@gmail.com>
2022-06-26 00:51:34 -0300

    modificacoes na css

--- produtos.js
+++ produtos.js
@@ -1,8 +1,24 @@
+// usar paramentros no auxilio do carrinho
+
 var produtos = [
 	// {desc: "", valor: "", cod: ""}
 	{desc: "Item 1", valor: "R$ 15, 00", cod: "01"},
 ]
 
-var divProdutos = document.querySelector()
+var divProdutos = document.querySelector('.produtos')
 
-console.log(produtos[0].desc)
+function gerarItens() {
+	for (var i = 0; i < 8; i++) {
+		divProdutos.innerHTML += `
+		<div class="produto">
+    			<img src="" class="img-produto"/>
+    			<p class="desc-p">Descricao do produto</p>
+    			<span class="span-p">
+    				<p class="valor-p">R$ 90, 00</p>
+    				<a href="#" class="add-cart">+</a>
+    			</span>
+    		</div>
+`
+	}
+}
+gerarItens()
